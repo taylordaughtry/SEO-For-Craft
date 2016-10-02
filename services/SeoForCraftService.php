c2d94596f99fcc29b8c6a57a6e2225b43fe3c2ec
Taylor Daughtry <hello@taylordaughtry.com>
2016-10-02 10:17:37 -0500

    Streamline field creation

--- services/SeoForCraftService.php
+++ services/SeoForCraftService.php
@@ -26,73 +26,95 @@ class SeoForCraftService extends BaseApplicationComponent
 
 	public function installFields($groupId)
 	{
-		$preview = new FieldModel();
-		$preview->groupId      = $groupId;
-		$preview->name         = 'Snippet Preview';
-		$preview->handle       = 'preview';
-		$preview->instructions = 'This is what your page could look like in search results.';
-		$preview->translatable = true;
-		$preview->type         = 'SeoForCraft_Preview';
-
-		if (! craft()->fields->saveField($preview))
-		{
-			Craft::log('Could not save the Preview field.', LogLevel::Warning);
-
-			return false;
-		}
-
-		$metaTitle = new FieldModel();
-		$metaTitle->groupId      = $groupId;
-		$metaTitle->name         = 'Meta Title';
-		$metaTitle->handle       = 'metaTitle';
-		$metaTitle->instructions = 'This will show in search results as the title of this page.';
-		$metaTitle->translatable = true;
-		$metaTitle->type         = 'PlainText';
-		$metaTitle->settings = array(
-			'maxLength' => '60'
+		$fields = array(
+			array(
+				'name' => 'Snippet Preview',
+				'handle' => 'preview',
+				'instructions' => 'This is what your page could look like in search results.',
+				'type' => 'SeoForCraft_Preview',
+				'settings' => false
+			),
+			array(
+				'name' => 'Meta Title',
+				'handle' => 'metaTitle',
+				'instructions' => 'This will show in search results as the title of this page.',
+				'type' => 'PlainText',
+				'settings' => array(
+					'maxLength' => '60'
+				)
+			),
+			array(
+				'name' => 'Meta Description',
+				'handle' => 'metaDescription',
+				'instructions' => 'This is a short snippet that teases the user about the content on this page. Note that google doesn\'t always use this.',
+				'type' => 'PlainText',
+				'settings' => array(
+					'maxLength' => '155'
+				)
+			),
+			array(
+				'name' => 'No Index',
+				'handle' => 'noIndex',
+				'instructions' => 'Ask search engines not to index this page.',
+				'type' => 'Lightswitch',
+				'settings' => array(
+					'on' => 'false'
+				)
+			),
+			array(
+				'name' => 'Open Graph Type',
+				'handle' => 'ogType',
+				'instructions' => 'What type should this entry be?',
+				'type' => 'Dropdown',
+				'settings' => array(
+					'options' => array(
+						array(
+							'label' => 'Article',
+							'value' => 'article'
+						),
+						array(
+							'label' => 'Website',
+							'value' => 'website'
+						)
+					)
+				)
+			),
+			array(
+				'name' => 'Open Graph Title',
+				'handle' => 'ogTitle',
+				'instructions' => 'If this is left blank, Craft will try the Meta Title, and finally default to the Entry Title.',
+				'type' => 'PlainText',
+				'settings' => array(
+					'maxLength' => '90'
+				)
+			),
+			array(
+				'name' => 'Open Graph Description',
+				'handle' => 'ogDescription',
+				'instructions' => 'This is displayed on Facebook when you share this page.',
+				'type' => 'PlainText',
+				'settings' => array(
+					'maxLength' => '200'
+				)
+			)
 		);
 
-		if (! craft()->fields->saveField($metaTitle))
-		{
-			Craft::log('Could not save the Meta Title field.', LogLevel::Warning);
+		foreach ($fields as $field) {
+			$model = new FieldModel();
+			$model->groupId      = $groupId;
+			$model->name         = $field['name'];
+			$model->handle       = $field['handle'];
+			$model->instructions = $field['instructions'];
+			$model->translatable = true;
+			$model->type         = $field['type'];
+			$model->settings = $field['settings'];
 
-			return false;
-		}
+			if (! craft()->fields->saveField($model))
+			{
+				Craft::log('Could not save the ' . $field['name'] . ' field.', LogLevel::Warning);
 
-		$metaDescription = new FieldModel();
-		$metaDescription->groupId      = $groupId;
-		$metaDescription->name         = 'Meta Description';
-		$metaDescription->handle       = 'metaDescription';
-		$metaDescription->instructions = 'This is a short snippet that teases the user about the content on this page. Note that google doesn\'t always use this.';
-		$metaDescription->translatable = true;
-		$metaDescription->type         = 'PlainText';
-		$metaDescription->settings = array(
-			'maxLength' => '155'
-		);
-
-		if (! craft()->fields->saveField($metaDescription))
-		{
-			Craft::log('Could not save the Meta Description field.', LogLevel::Warning);
-
-			return false;
-		}
-
-		$noIndex = new FieldModel();
-		$noIndex->groupId      = $groupId;
-		$noIndex->name         = 'No Index';
-		$noIndex->handle       = 'noIndex';
-		$noIndex->instructions = 'Ask search engines not to index this page.';
-		$noIndex->translatable = true;
-		$noIndex->type         = 'Lightswitch';
-		$noIndex->settings = array(
-			'on' => 'false'
-		);
-
-		if (! craft()->fields->saveField($noIndex))
-		{
-			Craft::log('Could not save the No Index field.', LogLevel::Warning);
-
-			return false;
+				return false;
+			}
 		}
 	}
 
